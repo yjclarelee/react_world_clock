48bb9f79508089de2ac8a63b5ce94b7e074eb3c0
yjclarelee <yjclarelee@postech.ac.kr>
2021-01-01 21:33:00 +0900

    Implemented real time clock for seoul

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 let btn_seoul = document.getElementById("seoul"),
     btn_berkeley = document.getElementById("berkeley"),
-    btn_new_york = document.getElementById("new-york")
-    body = document.body;
+    btn_new_york = document.getElementById("new-york"),
+    time = document.getElementById("time");
+
+// let fullTime = new Date(),
+//     hour = fullTime.getHours(),
+//     minute = fullTime.getMinutes(),
+//     sec = fullTime.getSeconds();
 
 btn_seoul.addEventListener("click", function changeColor(event){
     btn_seoul.style.backgroundColor = "blue";
@@ -20,6 +25,28 @@ btn_new_york.addEventListener("click", function changeColor(event){
     btn_berkeley.style.backgroundColor = "black";
     btn_new_york.style.backgroundColor = "blue";
 });
+
+function setTime(){
+    let fullTime = new Date(),
+        hour = fullTime.getHours(),
+        minute = fullTime.getMinutes(),
+        sec = fullTime.getSeconds();
+    if(hour < 10){
+        hour = '0' + hour;
+    }
+    if(minute < 10){
+        minute = '0' + minute;
+    }
+    if(sec < 10){
+        sec = '0' + sec;
+    }
+    time.innerText = `${hour} : ${minute} : ${sec}`;
+}
+
+// time.innerText = `${hour} : ${minute} : ${second}`;
+setTime();
+setInterval(setTime, 1000);
+
 
 // body.addEventListener("click", function changeColor(event){
 //     btn_seoul.style.backgroundColor = "black";
