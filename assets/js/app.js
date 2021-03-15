dd061422cbcac493759de91c8520dd8ad6aee017
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-03-15 17:09:00 +0900

    Refactor setTime function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,110 +1,114 @@
-let btn_seoul = document.getElementById("seoul"),
-    btn_berkeley = document.getElementById("berkeley"),
-    btn_new_york = document.getElementById("new-york");
-
-let time = document.getElementById("time");
+let btn_area = document.querySelector('.btn-collection');
+let time = document.querySelector(".time");
 
 // initalize time to Seoul
 setTime(0);
 let initial_setTime = setInterval(function(){setTime(0)}, 1000);
 
-// event listener for when the Seoul button is clicked
-btn_seoul.addEventListener("click", function (){
-    // set button color according to button click
-    btn_seoul.style.backgroundColor = "blue";
-    btn_berkeley.style.backgroundColor = "black";
-    btn_new_york.style.backgroundColor = "black";
 
-    // set seoul boxShadow
-    btn_seoul.style.boxShadow = "0px 0px 8px blue";
+btn_area.addEventListener('click', (event) => {
+    if(event.target.nodeName == 'BUTTON'){
+        changeTime(event.target.className);
+    }
+})
 
-    // clear inital time
-    clearInterval(initial_setTime);
-    // set inital time to seoul
-    setTime(0);
-    // start seoul time interval
-    let interval_seoul = setInterval(function(){setTime(0)}, 1000);
+function changeTime(city){
 
-    // when other buttons are clicked, remove the seoul time interval
-    btn_berkeley.addEventListener("click", function stop(){
-        clearInterval(interval_seoul);
-    });
-    btn_new_york.addEventListener("click", function stop(){
-        clearInterval(interval_seoul);
-    });
-});
+}
+// // event listener for when the Seoul button is clicked
+// btn_seoul.addEventListener("click", function (){
+//     // set button color according to button click
+//     btn_seoul.style.backgroundColor = "blue";
+//     btn_berkeley.style.backgroundColor = "black";
+//     btn_new_york.style.backgroundColor = "black";
 
-// event listener for when the Berkeley button is clicked
-btn_berkeley.addEventListener("click", function(){
-    // set button color according to button click
-    btn_seoul.style.backgroundColor = "black";
-    btn_berkeley.style.backgroundColor = "blue";
-    btn_new_york.style.backgroundColor = "black";
+//     // set seoul boxShadow
+//     btn_seoul.style.boxShadow = "0px 0px 8px blue";
 
-    // remove seoul boxShadow
-    btn_seoul.style.boxShadow = "0 0 #000";
+//     // clear inital time
+//     clearInterval(initial_setTime);
+//     // set inital time to seoul
+//     setTime(0);
+//     // start seoul time interval
+//     let interval_seoul = setInterval(function(){setTime(0)}, 1000);
 
-    // clear inital time
-    clearInterval(initial_setTime);
-    // set inital time to Berkeley
-    setTime(7);
-    // start Berkeley time interval
-    let interval_berkeley = setInterval(function(){setTime(7)}, 1000);
+//     // when other buttons are clicked, remove the seoul time interval
+//     btn_berkeley.addEventListener("click", function stop(){
+//         clearInterval(interval_seoul);
+//     });
+//     btn_new_york.addEventListener("click", function stop(){
+//         clearInterval(interval_seoul);
+//     });
+// });
 
-    // when other buttons are clicked, remove the Berkeley time interval
-    btn_seoul.addEventListener("click", function stop(){
-        clearInterval(interval_berkeley);
-    });
-    btn_new_york.addEventListener("click", function stop(){
-        clearInterval(interval_berkeley);
-    });
-});
+// // event listener for when the Berkeley button is clicked
+// btn_berkeley.addEventListener("click", function(){
+//     // set button color according to button click
+//     btn_seoul.style.backgroundColor = "black";
+//     btn_berkeley.style.backgroundColor = "blue";
+//     btn_new_york.style.backgroundColor = "black";
 
-// event listener for when the New York button is clicked
-btn_new_york.addEventListener("click", function(){
-    // set button color according to button click
-    btn_seoul.style.backgroundColor = "black";
-    btn_berkeley.style.backgroundColor = "black";
-    btn_new_york.style.backgroundColor = "blue";
+//     // remove seoul boxShadow
+//     btn_seoul.style.boxShadow = "0 0 #000";
 
-    // remove seoul boxShadow
-    btn_seoul.style.boxShadow = "0 0 #000";
+//     // clear inital time
+//     clearInterval(initial_setTime);
+//     // set inital time to Berkeley
+//     setTime(7);
+//     // start Berkeley time interval
+//     let interval_berkeley = setInterval(function(){setTime(7)}, 1000);
 
-    // clear inital time
-    clearInterval(initial_setTime);
-    // set inital time to New York
-    setTime(10);
-    // start New York time interval
-    let interval_new_york = setInterval(function(){setTime(10)}, 1000);
+//     // when other buttons are clicked, remove the Berkeley time interval
+//     btn_seoul.addEventListener("click", function stop(){
+//         clearInterval(interval_berkeley);
+//     });
+//     btn_new_york.addEventListener("click", function stop(){
+//         clearInterval(interval_berkeley);
+//     });
+// });
 
-    // when other buttons are clicked, remove the New York time interval
-    btn_seoul.addEventListener("click", function stop(){
-        clearInterval(interval_new_york);
-    });
-    btn_berkeley.addEventListener("click", function stop(){
-        clearInterval(interval_new_york);
-    });
-});
+// // event listener for when the New York button is clicked
+// btn_new_york.addEventListener("click", function(){
+//     // set button color according to button click
+//     btn_seoul.style.backgroundColor = "black";
+//     btn_berkeley.style.backgroundColor = "black";
+//     btn_new_york.style.backgroundColor = "blue";
+
+//     // remove seoul boxShadow
+//     btn_seoul.style.boxShadow = "0 0 #000";
+
+//     // clear inital time
+//     clearInterval(initial_setTime);
+//     // set inital time to New York
+//     setTime(10);
+//     // start New York time interval
+//     let interval_new_york = setInterval(function(){setTime(10)}, 1000);
+
+//     // when other buttons are clicked, remove the New York time interval
+//     btn_seoul.addEventListener("click", function stop(){
+//         clearInterval(interval_new_york);
+//     });
+//     btn_berkeley.addEventListener("click", function stop(){
+//         clearInterval(interval_new_york);
+//     });
+// });
 
 function setTime(num){
     // get current time
-    let fullTime = new Date(),
-        hour = (fullTime.getHours()+num) % 24,
-        minute = fullTime.getMinutes(),
-        sec = fullTime.getSeconds();
+    let fullTime = new Date();
+    let hour = (fullTime.getHours()+num) % 24;
+    let minute = fullTime.getMinutes();
+    let sec = fullTime.getSeconds();
 
     // if the digit is single, put 0 before it
-    if(hour < 10){
-        hour = '0' + hour;
-    }
-    if(minute < 10){
-        minute = '0' + minute;
-    }
-    if(sec < 10){
-        sec = '0' + sec;
-    }
-
-    // set the innerText of the time
+    hour = padZero(hour);
+    minute = padZero(minute);
+    sec = padZero(sec);
+    
     time.innerText = `${hour} : ${minute} : ${sec}`;
-    console.log(num);
+}
+
+function padZero(num){
+    if(num > 9) return num;
+    else return '0' + num;
 }
